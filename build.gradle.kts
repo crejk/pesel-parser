295373310d7b8d0fdd2cc62b320b8d36b5465fbb
crejk <crejk@ctemplar.com>
2020-08-03 02:44:58 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    kotlin("jvm") version "1.3.72"
+}
+
+group = "pl.crejk"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:4.1.3")
+    testImplementation("io.kotest:kotest-runner-console-jvm:4.1.3")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
