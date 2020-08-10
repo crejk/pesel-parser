53790167645b7effdd2353a63f31cd0303faa2e1
crejk <crejk@ctemplar.com>
2020-08-11 01:02:52 +0200

    add useful functions to Validated type

--- src/main/kotlin/pl/crejk/pesel/parser/fp/Validated.kt
+++ src/main/kotlin/pl/crejk/pesel/parser/fp/Validated.kt
@@ -1,5 +1,7 @@
 package pl.crejk.pesel.parser.fp
 
+import java.util.*
+
 sealed class Validated<out E, out V> {
 
     data class Invalid<E>(val error: E) : Validated<E, Nothing>()
@@ -10,16 +12,47 @@ sealed class Validated<out E, out V> {
         is Invalid -> this
     }
 
-    fun fold(ifInvalid: (E) -> Unit, ifValid: (V) -> Unit): Unit = when (this) {
-        is Valid -> ifValid(this.value)
-        is Invalid -> ifInvalid(this.error)
+    fun unsafe(): V = when (this) {
+        is Valid -> this.value
+        is Invalid -> throw IllegalStateException("$this is not valid instance")
     }
+
+    fun orNull(): V? = this.getOrElse { null }
 }
 
 fun <V> V.valid(): Validated<Nothing, V> = Validated.Valid(this)
 fun <E> E.invalid(): Validated<E, Nothing> = Validated.Invalid(this)
 
+inline fun <E, V, T : V> Validated<E, T>.getOrElse(ifInvalid: (E) -> T): V = when (this) {
+    is Validated.Valid -> this.value
+    is Validated.Invalid -> ifInvalid(this.error)
+}
+
+inline fun <V, E, T> Validated<E, V>.fold(ifInvalid: (E) -> T, ifValid: (V) -> T): T = when (this) {
+    is Validated.Valid -> ifValid(this.value)
+    is Validated.Invalid -> ifInvalid(this.error)
+}
+
+inline fun <E, V> Validated<E, V>.onValid(f: (V) -> Unit): Validated<E, V> = when(this) {
+    is Validated.Valid -> {
+        f(this.value)
+        this
+    }
+    is Validated.Invalid -> this
+}
+
+inline fun <E, V> Validated<E, V>.onInvalid(f: (E) -> Unit): Validated<E, V> = when(this) {
+    is Validated.Valid -> this
+    is Validated.Invalid -> {
+        f(this.error)
+        this
+    }
+}
+
 inline fun <E, V, M> Validated<E, V>.flatMap(f: (V) -> Validated<E, M>) = when (this) {
     is Validated.Valid -> f(this.value)
     is Validated.Invalid -> this
 }
+
+fun <E, V> Validated<E, V>.toOptional(): Optional<V> =
+    this.fold({ Optional.empty() }, { Optional.of(it) })
